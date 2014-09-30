da3dbf16fbc2b71eca394ce6eff17ec2d70f6659
Jonathan Ong <jonathanrichardong@gmail.com>
2014-09-29 22:53:17 -0700

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# paas
+
+[![NPM version][npm-image]][npm-url]
+[![Build status][travis-image]][travis-url]
+[![Test coverage][coveralls-image]][coveralls-url]
+[![Dependency Status][david-image]][david-url]
+[![License][license-image]][license-url]
+[![Downloads][downloads-image]][downloads-url]
+[![Gittip][gittip-image]][gittip-url]
+
+[npm-image]: https://img.shields.io/npm/v/paas.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/paas
+[github-tag]: http://img.shields.io/github/tag/polyfills/paas.svg?style=flat-square
+[github-url]: https://github.com/polyfills/paas/tags
+[travis-image]: https://img.shields.io/travis/polyfills/paas.svg?style=flat-square
+[travis-url]: https://travis-ci.org/polyfills/paas
+[coveralls-image]: https://img.shields.io/coveralls/polyfills/paas.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/r/polyfills/paas
+[david-image]: http://img.shields.io/david/polyfills/paas.svg?style=flat-square
+[david-url]: https://david-dm.org/polyfills/paas
+[license-image]: http://img.shields.io/npm/l/paas.svg?style=flat-square
+[license-url]: LICENSE
+[downloads-image]: http://img.shields.io/npm/dm/paas.svg?style=flat-square
+[downloads-url]: https://npmjs.org/package/paas
+[gittip-image]: https://img.shields.io/gratipay/jonathanong.svg?style=flat-square
+[gittip-url]: https://gratipay.com/jonathanong/
---
+++ test/test.js
@@ -0,0 +1,136 @@
+
+var request = require('supertest')
+var assert = require('assert')
+
+require('polyfills')().clean()
+
+var server = require('..').listen()
+
+// http://caniuse.com/#feat=promises
+// http://www.useragentstring.com/pages/Chrome/
+var chrome27 = 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1500.55 Safari/537.36'
+var chrome35 = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+var etag
+
+describe('PaaS', function () {
+  it('GET /', function (done) {
+    request(server)
+    .get('/')
+    .expect(404, done)
+  })
+
+  it('HEAD /polyfill.js', function (done) {
+    request(server)
+    .head('/polyfill.js')
+    .expect(200, function (err, res) {
+      assert.ifError(err)
+      assert(etag = res.headers.etag)
+      done()
+    })
+  })
+
+  it('OPTIONS /polyfill.js', function (done) {
+    request(server)
+    .options('/polyfill.js')
+    .expect('Allow', 'OPTIONS,HEAD,GET')
+    .expect(204, done)
+  })
+
+  it('POST /polyfill.js', function (done) {
+    request(server)
+    .post('/polyfill.js')
+    .expect('Allow', 'OPTIONS,HEAD,GET')
+    .expect(405, done)
+  })
+
+  describe('GET /polyfill.js', function () {
+    it('should respect Accept-Encoding: identity', function (done) {
+      request(server)
+      .get('/polyfill.js')
+      .set('Accept-Encoding', 'identity')
+      .expect(200, function (err, res) {
+        assert.ifError(err)
+        assert(!res.headers['content-encoding'])
+        done()
+      })
+    })
+
+    it('should support 304 status codes with ETags', function (done) {
+      request(server)
+      .get('/polyfill.js')
+      .set('If-None-Match', etag)
+      .expect(304, done)
+    })
+
+    it('should not include promises with Chrome 35', function (done) {
+      request(server)
+      .get('/polyfill.js')
+      .set('User-Agent', chrome35)
+      .expect('Content-Type', /application\/javascript/)
+      .expect('Content-Encoding', 'gzip')
+      .expect(200, function (err, res) {
+        assert.ifError(err)
+        var js = res.text
+        new Function(js)
+        assert(!~js.indexOf('Promise'))
+        done()
+      })
+    })
+
+    it('should include promises with Chrome 27', function (done) {
+      request(server)
+      .get('/polyfill.js')
+      .set('User-Agent', chrome27)
+      .expect('Content-Type', /application\/javascript/)
+      .expect(200, function (err, res) {
+        assert.ifError(err)
+        var js = res.text
+        new Function(js)
+        assert(~js.indexOf('Promise'))
+        done()
+      })
+    })
+
+    it('should not include promises w/ Chrome 27 and include!=promise', function (done) {
+      request(server)
+      .get('/polyfill.js?include=domels')
+      .set('User-Agent', chrome27)
+      .expect('Content-Type', /application\/javascript/)
+      .expect(200, function (err, res) {
+        assert.ifError(err)
+        var js = res.text
+        new Function(js)
+        assert(!~js.indexOf('Promise'))
+        done()
+      })
+    })
+
+    it('should not include promises w/ Chrome 27 and exclude=promise', function (done) {
+      request(server)
+      .get('/polyfill.js?exclude=promise')
+      .set('User-Agent', chrome27)
+      .expect('Content-Type', /application\/javascript/)
+      .expect(200, function (err, res) {
+        assert.ifError(err)
+        var js = res.text
+        new Function(js)
+        assert(!~js.indexOf('Promise'))
+        done()
+      })
+    })
+
+    it('should support , and ; delimited options', function (done) {
+      request(server)
+      .get('/polyfill.js?include=promise;domelements;raf')
+      .expect(200, function (err, res) {
+        assert.ifError(err)
+        var js = res.text
+        new Function(js)
+        assert(~js.indexOf('Promise'))
+        assert(~js.indexOf('Element.prototype.query'))
+        assert(~js.indexOf('requestAnimationFrame'))
+        done()
+      })
+    })
+  })
+})
